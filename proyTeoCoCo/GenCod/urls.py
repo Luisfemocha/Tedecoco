098478127bbaf9a774d591ec9a812c0c4442074a
Juan Felipe Valencia <juanfevalenciac@gmail.com>
2020-09-15 07:28:08 -0500

    de collab a Django

--- proyTeoCoCo/GenCod/urls.py
+++ proyTeoCoCo/GenCod/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('inicio',views.inicio,name='inicio'),
+    path('conceptos',views.conceptos,name='conceptos'),
+    path('notas',views.notas,name='notas'),
+    path('conexiones',views.conexiones,name='conexiones'),
+    #path('comprobar',views.comprobar,name='comprobar'),
 ]
--- proyTeoCoCo/GenCod/views.py
+++ proyTeoCoCo/GenCod/views.py
@@ -30,3 +30,41 @@ def inicio(request):
     json_pretty = json.dumps(inicio, sort_keys=True, indent=4)
     #return HttpResponse(json.dumps(inicio['mxGraphModel']['root']['mxCell'],indent=4))
     return render(request, "listaObjetos.html", {'jsonList': inicio['mxGraphModel']['root']['mxCell']})
+
+def conceptos(request):
+    Aux=request.POST['contenidoJson']
+    Aux=Aux.replace("\'", "\"")
+    A=json.loads(Aux)
+    lista=[]
+    for i in range(2, len(A)):
+        if "rounded=0" in A[i]['@style'] and "dashed=1" not in A[i]['@style'] and "edgeStyle=orthogonalEdgeStyle" not in A[i]['@style'] and "@value" in A[i] and A[i]['@value'] != "":
+            lista.append(A[i]["@value"])
+    return render(request,"listaConceptos.html",{'listaConceptos':lista})
+
+
+def notas(request):
+    Aux=request.POST['contenidoJson']
+    Aux=Aux.replace("\'", "\"")
+    A=json.loads(Aux)
+    lista=[]
+    for i in range(2, len(A)):
+        if "rounded=0" in A[i]['@style'] and "dashed=1" in A[i]['@style'] and "edgeStyle=orthogonalEdgeStyle" not in A[i]['@style'] and "@value" in A[i]:
+            lista.append(A[i]["@value"])
+    return render(request,"listaNotas.html",{'listaConceptos':lista})
+
+def conexiones(request):
+    Aux=request.POST['contenidoJson']
+    Aux=Aux.replace("\'", "\"")
+    A=json.loads(Aux)
+    lista=[] #tupla id,desde, hasta
+    flechas=[]
+    def dato(id,A):
+        for i in range(2, len(A)):
+            if id in A[i]["@id"]:
+                return A[i]["@value"]
+    
+    for i in range(2, len(A)):
+        if "edgeStyle=orthogonalEdgeStyle" in A[i]['@style']:
+          lista.append((A[i]["@source"],dato(A[i]["@source"],A),dato(A[i]["@target"],A)))
+    return render(request,"listaConexiones.html",{'listaConceptos':lista})
+
